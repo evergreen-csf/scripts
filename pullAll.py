a4498056d0d2ff1974c5468241cb46e5a8187ca9
Paul Pham <ppham@local-box.org>
2014-03-15 17:49:35 -0700

    Fixed pathnames of data files to look in staff repo Needs to be cloned separately at the same level as scripts.

--- pullAll.py
+++ pullAll.py
@@ -1,7 +1,10 @@
 from subprocess import Popen
 import names, sys, os.path
 
-
 for student in names.student_info_field("email"):
-    Popen(["echo ", "grading/"+student, ";", "cd","grading/"+student,";","git","pull"])
+  print("grading/"+student)
+  try:
+    Popen(["cd","grading/"+student,";","git","pull"])
+  except:
+    print("Couldn't open grading/"+student)
 
