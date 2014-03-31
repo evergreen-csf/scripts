f4b838015073ff45fa632d9709bc777a42ced3be
Colin Robinson <robcol15@evergreen.edu>
2014-03-30 21:10:40 -0700

    update mkdirs_launcher to reflect changes to names.py

--- mkdirs_launcher.py
+++ mkdirs_launcher.py
@@ -1,6 +1,6 @@
 from subprocess import Popen
 import names
 
-for student in names.emails:
+for student in names.student_info_field("email"):
   #altered for Spring quarter
   Popen(["./spring_mkdirs", student, "../grading/"])
