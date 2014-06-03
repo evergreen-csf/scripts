49816108b01389872f6dd51df17a3df76a7b53b4
colin <colinarobinson@gmail.com>
2014-06-03 00:36:48 -0700

    added arch and prog dirs for organization

--- mkdirs_launcher.py
+++ mkdirs_launcher.py
@@ -1,6 +1,6 @@
 from subprocess import Popen
 import names
 
-for student in ("model"): # names.student_info_field("email"):
+for student in ("model"): #names.student_info_field("email"):
   #altered for Spring quarter
-  Popen(["./spring_mkdirs", student, "../grading/"])
+  Popen(["./mkdirs", student, "../repos/"])
