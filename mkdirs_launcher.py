8124fec29145ce1aad4da6336057955894f6e6de
colin <colinarobinson@gmail.com>
2014-06-03 00:45:39 -0700

    script error, sorry

--- mkdirs_launcher.py
+++ mkdirs_launcher.py
@@ -1,6 +1,6 @@
 from subprocess import call
 import names
 
-for student in names.student_info_field("email"):
+for student in ["model"]: # names.student_info_field("email"):
   #altered for Spring quarter
   call(["./mkdirs", student, "../repos/"])
